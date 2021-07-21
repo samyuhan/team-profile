dc56c66efd62a78a9b32804ee34706b777c38ff4
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-20 22:08:39 -0700

    add employee prompt questions

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const team = [];
+
 // Questions to ask 
-const questions = () => {
+const managerQuestions = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -16,16 +21,84 @@ const questions = () => {
         },
         {
             type: 'input',
-             name: 'email',
-             message: "What is the manager's email?"
+            name: 'email',
+            message: "What is the manager's email?"
         },
         {
             type: 'input',
-            name: 'office',
+            name: 'officeNumber',
             message: "What is the manager's office number?"
 
         }
     ])
-}
+        .then(managerAnswers => {
+            const { name, id, email, officeNumber } = managerAnswers;
+            const manager = new Manager(name, id, email, officeNumber);
 
-questions();
+            team.push(manager);
+        })
+};
+
+const employeeQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is your employee's role?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the employee's Github?",
+            when: (input) => input.role === "Engineer"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's school?",
+            when: (input) => input.role === "Intern"
+        },
+        {
+            type: 'confirm',
+            name: 'confirm',
+            message: 'Do you want to add anyone else?',
+            default: false
+        }
+    ])
+        .then(employeeAnswers => {
+            let { name, id, email, role, github, school, confirm } = employeeAnswers;
+            let employee;
+
+            if (role === "Engineer") {
+                employee = new Engineer(name, id, email, github);
+            } else if (role === "Intern") {
+                employee = new Intern(name, id, email, school);
+            }
+
+            team.push(employee);
+
+            if (confirm) {
+                return employeeQuestions(team);
+            } else {
+                return team;
+            }
+        })
+};
+
+managerQuestions();
